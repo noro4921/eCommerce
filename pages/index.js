ae17f7707d3629c7c60a3b41731beaccfc23affe
Reuben Khadka <noro4921@gmail.com>
2022-10-30 14:33:27 +1100

    Added product details and url links and herobanner

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,25 @@ import React from "react";
 
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-import client from "../lib/client";
+import { client } from "../lib/client";
 
-const Home = () => {
-  return (
-    <>
-      <HeroBanner />
-      <div className="products-heading">
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
-      <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
-      </div>
-      <FooterBanner />
-    </>
-  );
-};
+    <div className="products-heading">
+      <h2>Best Selling Products</h2>
+      <p>Speakers of many variations</p>
+    </div>
+
+    <div className="products-container">
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
@@ -29,10 +30,7 @@ export const getServerSideProps = async () => {
   const bannerData = await client.fetch(bannerQuery);
 
   return {
-    props: {
-      products,
-      bannerData,
-    },
+    props: { products, bannerData },
   };
 };
 
--- sanity_ecommerce/schemas/product.js
+++ sanity_ecommerce/schemas/product.js
@@ -1,5 +1,5 @@
 export default {
-  name: "prodcut",
+  name: "product",
   title: "Product",
   type: "document",
   fields: [
